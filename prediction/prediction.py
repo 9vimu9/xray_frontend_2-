201649851c346a33ba0d7918d1e8bd69ebc6967b
vimukthi <vimukthi@efuturesworld.com>
2020-03-21 03:47:47 +0530

    add try catch to whole

--- prediction/prediction.py
+++ prediction/prediction.py
@@ -1,38 +1,43 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-import tensorflow as tf
-from PIL import Image
-import os
-import numpy as np 
-import sys
+
+try:
+    import tensorflow as tf
+    from PIL import Image
+    import os
+    import numpy as np 
+    import sys
 
 
-TEST_IMAGE_PATH = sys.argv[1]
-MODEL_PATH = "model_pretrain.h5"
-IMG_SIZE = 64
-LABELS = ['BACTERIA','NORMAL','VIRUS']
+    TEST_IMAGE_PATH = sys.argv[1]
+    MODEL_PATH = "model_pretrain.h5"
+    IMG_SIZE = 64
+    LABELS = ['BACTERIA','NORMAL','VIRUS']
 
-new_model = tf.keras.models.load_model(MODEL_PATH)
+    new_model = tf.keras.models.load_model(MODEL_PATH)
 
-def decode_img(image_path):
-    im = Image.open(image_path).convert('RGB')
-    new_size = (IMG_SIZE,IMG_SIZE)
-    im = im.resize(new_size, Image.ANTIALIAS)
-    data = np.asarray(im)
-    image = np.array(data)
-    image = np.divide(image , 255.0)
-    return image
+    def decode_img(image_path):
+        im = Image.open(image_path).convert('RGB')
+        new_size = (IMG_SIZE,IMG_SIZE)
+        im = im.resize(new_size, Image.ANTIALIAS)
+        data = np.asarray(im)
+        image = np.array(data)
+        image = np.divide(image , 255.0)
+        return image
 
 
-img = decode_img(TEST_IMAGE_PATH)
+    img = decode_img(TEST_IMAGE_PATH)
 
-img = np.expand_dims(img, axis=0)
+    img = np.expand_dims(img, axis=0)
 
-result = new_model.predict(img)
-maxValue = result.argmax()
-values = result.flatten()
-print(values)
-print(maxValue)
-print(LABELS[maxValue])
+    result = new_model.predict(img)
+    maxValue = result.argmax()
+    values = result.flatten()
+    print(values)
+    print(maxValue)
+    print(LABELS[maxValue])
 
-#print(result)
-# print(img.shape)
+    #print(result)
+    # print(img.shape)
+
+except Exception as e:
+    print('Failed to open file: %s' % (e,))
